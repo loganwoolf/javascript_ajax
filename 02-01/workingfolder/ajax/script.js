bcb562c4dba92db4fac118195d61adcd3926bbf6
Logan Woolf <logan.woolf@arcticleaf.io>
2022-07-05 11:25:15 -0600

    Write responseText to document

---
+++ 02-01/workingfolder/ajax/script.js
@@ -0,0 +1,13 @@
+var request;
+if (window.XMLHttpRequest) {
+	request = new XMLHttpRequest();
+} else {
+	request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+request.open('GET', 'data.txt');
+request.onreadystatechange = function() {
+	if ((request.readyState===4) && (request.status===200)) {
+		document.querySelector('#target').textContent = request.responseText
+	}
+}
+request.send();
