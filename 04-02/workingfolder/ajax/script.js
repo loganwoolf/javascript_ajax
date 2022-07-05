522849d8437b326fe342da1f4e0b249af683e7e1
Logan Woolf <logan.woolf@arcticleaf.io>
2022-07-05 15:34:31 -0600

    Add search functionality to elements

--- 04-02/workingfolder/ajax/script.js
+++ 04-02/workingfolder/ajax/script.js
@@ -1,16 +1,39 @@
 $(() => {
-  $.getJSON('data.json', (data) => {
-    let $ul = $('<ul>')
-    $.each(data, (key, val) => {
-      console.log(val.shortname)
-      let $li = $('<li>')
-      $('<h2>').text(val.name).appendTo($li)
-      $('<img>').attr({
-        src: `./images/${val.shortname}_tn.jpg`,
-        alt: val.name
-      }).appendTo($li)
-      $li.appendTo($ul)
-    })
-    $($ul).appendTo($('#update'))
+  $('#search').on({
+    input: getResults
   })
+
+  function getResults(params) {
+    const searchString = $(this).val()
+    console.log()
+    const regex = new RegExp(searchString, 'i')
+
+    $.getJSON('data.json', (data) => {
+      // create UL element
+      let $ul = $('<ul>')
+      // loop over data items
+      $.each(data, (index, value) => {
+        // clear any items that exist after each search
+        $('#update').empty()
+
+        // set conditions to accept item
+        if (searchString.length > 0 && value.name.search(regex) !== -1 || value.bio.search(regex) !== -1) {
+
+          // create list item and children
+          let $li = $('<li>')
+          $('<h2>').text(value.name).appendTo($li)
+          $('<img>').attr({
+            src: `./images/${value.shortname}_tn.jpg`,
+            alt: value.name
+          }).appendTo($li)
+          $('<p>').text(value.bio).appendTo($li)
+
+          // append LI to UL
+          $li.appendTo($ul)
+        }
+      })
+      // mount UL to div
+      $($ul).appendTo($('#update'))
+    })
+  }
 })
